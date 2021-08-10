eb702eac9c183cec8bf2e0e80d7c08a6ed9bc8b2
dcravenus <dcravenus@gmail.com>
2021-08-10 10:30:04 -0600

    Update

--- main.sh
+++ main.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-version="1.0.1"
+version="1.0.2"
 echo $version
+latest_version=`git ls-remote -t git@github.com:dcravenus/tagging-test.git | sed 's/.*refs\/tags\/\(.*\)/\1/' | tail -1`
+echo $latest_version
+
+if [ "$version" != "$latest_version" ]; then
+  echo 'update please'
+fi
